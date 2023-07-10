27a9d6094638f7898ebd054a9b7eef435376e69a
Parthmandke <129390094+Parthmandke@users.noreply.github.com>
2023-07-10 12:06:14 +0530

    pawan saved

--- src/imarticus/hello.java
+++ src/imarticus/hello.java
@@ -3,7 +3,7 @@ package imarticus;
 public class hello {
 
 	public static void main(String[] args) {
-	System.out.println("this is my project");
+	System.out.println("this is my project this is pawan project");
 
 	}
 
